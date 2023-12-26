b225615bdd8db0b1f8e99b7a31ebf36f341cb84f
roni <codingloaded@gmail.com>
2023-12-26 10:31:33 +0530

    threadlist page updated and question asking form added to it

--- threadlist.php
+++ threadlist.php
@@ -36,6 +36,25 @@
         </div>
     </div>
 
+    <div class="container">
+        <h3 class="text-center">Ask a question</h3>
+        <form action=" ">
+            <div class="form-group my-3">
+                <div class="mb-3">
+                    <label for="threads_name" class="form-label">Question</label>
+                    <input type="text" class="form-control" id="threads_name" name="threads_name"
+                        placeholder="Write your question">
+                </div>
+                <div class="mb-3">
+                    <label for="threads_desc" class="form-label">Elaborate your question</label>
+                    <textarea class="form-control" id="threads_desc" name="threads_desc" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+
+    </div>
+
     <div class="container my-3">
         <h3 class="my-3 text-center">Browse Question</h3>
 
@@ -43,38 +62,38 @@
             $id = $_GET['catid'];
             $sql = "SELECT * FROM `threads` WHERE `threads_cat_id` = $id";
             $result = mysqli_query($conn, $sql);
+            $noResult = true;
             while($row = mysqli_fetch_assoc($result)){
                 $threads_name =  $row["threads_name"];
                 $threads_id = $row["threads_id"];
                 $threads_desc =  $row["threads_desc"];
+                $noResult = false;
            
-    
-
-               echo '<div class="question">
+             echo '<div class="question">
                     <img src="partials/user.png" alt="">
                     <div>
-                        <h6><a class = "text-dark" href = "thread.php?tid='.$threads_id.'">'.$threads_name.'</a></h6>
+                        <h6><a class = "text-primary" href = "thread.php?tid='.$threads_id.'">'.$threads_name.'</a></h6>
                         <p>'.$threads_desc.'</p>
                     </div>
                 </div>';
             }
+
+            if($noResult){
+                echo '<p class ="question center">No question till now, be the first one to ask</P';
+            }
+
+
         ?>
-        <!-- will delete later  -->
-        <div class="question">
-            <img src="partials/user.png" alt="">
-            <div>
-                <h6>Media Heading</h6>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, aliquam.</p>
-            </div>
-        </div>
-        
 
 
-    <?php include 'partials/_footer.php'; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+
+
+        <?php include 'partials/_footer.php'; ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
 
 </body>
 
