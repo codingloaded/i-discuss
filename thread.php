81902d5cbc381a9d16f10245f2353cd90f82bcf1
roni <codingloaded@gmail.com>
2024-01-02 10:18:08 +0530

    Showing answers on thread.php

--- thread.php
+++ thread.php
@@ -14,8 +14,9 @@
     <?php include 'partials/_nav.php'; ?>
     <?php include 'partials/_dbconnect.php'; ?>
     <?php
-     $id = $_GET['tid'];
-     $sql =  $sql = "SELECT * FROM `threads` WHERE `threads_id` = $id";
+     $tid = $_GET['tid'];
+     echo $tid;
+     $sql =  $sql = "SELECT * FROM `threads` WHERE `threads_id` = $tid";
             $result = mysqli_query($conn, $sql);
             while($row = mysqli_fetch_assoc($result)){
                 $threads_name =  $row["threads_name"];
@@ -36,31 +37,53 @@
         </div>
     </div>
 
+    <div class="container">
+        <h3 class="text-center">Post your answer</h3>
+        <form action="<?php $_SERVER['REQUEST_URI']?>" method="post">
+            <!-- Here 46 $id can not be used as for is in html so request_uri has been used  -->
+            <div class="form-group my-3">
+                
+                <div class="mb-3">
+                    <label for="comment_content" class="form-label">Write your answer</label>
+                    <textarea class="form-control" id="comment_content" name="comment_content" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+
+    </div>
+
+
     <div class="container my-3">
         <h3 class="my-3 text-center">Discussion</h3>
 
-        <!-- its demo php will be deleted later  -->
-        <!-- <?php
-            $id = $_GET['tid'];
-            $sql = "SELECT * FROM `threads` WHERE `threads_id` = $id";
+       <?php
+           $tid = $_GET['tid'];
+            $sql = "SELECT * FROM `comments` WHERE `thread_id` = $tid";
             $result = mysqli_query($conn, $sql);
+            $noResult = true;
             while($row = mysqli_fetch_assoc($result)){
-                $threads_name =  $row["threads_name"];
-                $threads_cat_id = $row["threads_cat_id"];
-                $threads_desc =  $row["threads_desc"];
+                $comment_content =  $row["comment_content"];
+                $comment_id = $row["comment_id"];
+                $user_id =  $row["user_id"];
+                $noResult = false;
            
-    
-
-               echo '<div class="question">
-                    <img src="partials/user.png" alt="">
-                    <div>
-                        <h6><a class = "text-dark" href = "threads.php">'.$threads_name.'</a></h6>
-                        <p>'.$threads_desc.'</p>
-                    </div>
-                </div>';
+             echo '<div class="question">
+             <img src="partials/user.png" alt="">
+             <div>
+                 <h6> Posted by:- Anirban Ghosal </h6>
+                 <p>'.$comment_content.'</p>
+             </div>
+         </div>';;
             }
-        ?> -->
-    
+
+            if($noResult){
+                echo '<p class ="question center">No question till now, be the first one to ask</P';
+            }
+
+
+        ?>
+    </div>
         
 
 
